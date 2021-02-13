aae8566cf2d3446d17899adddc4166813e992400
Danaproject <decoratio@gmail.com>
2021-02-13 17:22:28 +0100

    task done

--- src/color-switch.js
+++ src/color-switch.js
@@ -6,3 +6,34 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+}
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+let IntervalId;
+function onStart() {
+  if (refs.startBtn.classList.contains('isActive')) return;
+  refs.startBtn.classList.add('isActive');
+  IntervalId = setInterval(() => {
+    let randomIndex = randomIntegerFromInterval(0, colors.length-1);
+    refs.body.style.backgroundColor = colors[randomIndex];
+    // console.log(randomIndex);
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(IntervalId);
+  refs.startBtn.classList.remove('isActive');
+  // console.log('Interval cleared');
+}
+console.log(refs)
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
